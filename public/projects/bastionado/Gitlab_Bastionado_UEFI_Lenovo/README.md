f32c9929371231624da1fa63795d4110762a23bb
Jose <jeffthehacker88210@gmail.com>
2025-11-08 15:51:22 +0100

    minor reestructuring

---
+++ public/projects/bastionado/Gitlab_Bastionado_UEFI_Lenovo/README.md
@@ -0,0 +1,1 @@
+Proyecto de guía de Bastionado de una UEFI de portátil Lenovo en formato Gitlab
--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     id: 1,
     title: "Guia Bastionado UEFI en Codelab",
     description: "Guía paso por paso",
-    url: "https://midex882.github.io/projects/bastionado/Gitlab_Bastionado_UEFI_Lenovo/index.html",
+    url: "${import.meta.env.BASE_URL}projects/bastionado/Gitlab_Bastionado_UEFI_Lenovo/index.html",
     image: "/images/sample1.png"
   },
   {
